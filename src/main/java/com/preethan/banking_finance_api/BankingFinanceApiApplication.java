0356ca91b4fe0a5e3f76d570e0f515bb18ea9968
Preethan M Shet <preethanmshet@Preethans-MacBook-Air.local>
2026-06-05 22:13:51 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/preethan/banking_finance_api/BankingFinanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.preethan.banking_finance_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingFinanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingFinanceApiApplication.class, args);
+	}
+
+}
