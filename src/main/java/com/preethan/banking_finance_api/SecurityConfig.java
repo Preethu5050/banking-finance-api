0acc721b74647fcd2f224538dd4afef0a6435738
Preethan M Shet <preethanmshet@Preethans-MacBook-Air.local>
2026-06-10 02:14:55 +0530

    Fix security config

--- src/main/java/com/preethan/banking_finance_api/SecurityConfig.java
+++ src/main/java/com/preethan/banking_finance_api/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
-            .sessionManagement(session -> 
+            .sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/**").permitAll()
